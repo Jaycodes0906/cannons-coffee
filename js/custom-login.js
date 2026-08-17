020890916a79f3a7b7aa14d427cd26f141ccc57f
Jay0906 <jordan81hoc@gmail.com>
2026-08-17 17:06:11 -0400

    changed some codes

--- js/custom-login.js
+++ js/custom-login.js
@@ -1,14 +1,12 @@
 const form = document.getElementById("inquiryForm");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", submit)
+    function(event) {
     event.preventDefault();
 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const pass = document.getElementById("pass").value;
-
-    const inquiryChoice = inquiry ? inquiry.value: "No selection";
-
     
     console.log("Name:", name);
     console.log("Email:", email);
@@ -17,6 +15,4 @@ form.addEventListener("submit", function(event) {
 
     message.textContent = `Welcome, ${name}! You are logged in!`
     console.log(message);
-
-    setTimeout(function(){
-        message.textContent = "";
+}
