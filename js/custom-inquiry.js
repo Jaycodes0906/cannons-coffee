081a4ae9ac0f1a02c0f0dfed31b74a1097fc03f1
Jay0906 <jordan81hoc@gmail.com>
2026-08-17 18:35:01 -0400

    changed a word in js

--- js/custom-inquiry.js
+++ js/custom-inquiry.js
@@ -23,13 +23,13 @@ function onFormSubmit(event) {
     
     console.log("Name:", name);
     console.log("Email:", email);
-   console.log("tel", phoneNumber);
+   console.log("phone", phone);
    console.log("comments", comments);
    console.log("inquiry", inquiry);
    console.log("coffee", coffee);
    
 
-    message.textContent = `Thank you ${name}! We appreciate your interest in ${inquiry}. Your coffee flavor is ${coffee} and your comments are ${comments}. We will soon contact you at ${email} or ${phoneNumber}.`
+    message.textContent = `Thank you ${name}! We appreciate your interest in ${inquiry}. Your coffee flavor is ${coffee} and your comments are ${comments}. We will soon contact you at ${email} or ${phone}.`
     console.log(message);
 }
 
