4f087afd17e229cfc31a30682822a7ebbc6e5eec
Jay0906 <jordan81hoc@gmail.com>
2026-08-16 20:37:49 -0400

    add inquiry html and login html and coustom-inquiry and login.js

---
+++ js/custom-login.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("inquiryForm");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("pass").value;
+
+    const inquiryChoice = inquiry ? inquiry.value: "No selection";
+
+    
+    console.log("Name:", name);
+    console.log("Email:", email);
+   console.log("password", pass);
+
+
+    message.textContent = `Welcome, ${name}! You are logged in!`
+    console.log(message);
+
+    setTimeout(function(){
+        message.textContent = "";
