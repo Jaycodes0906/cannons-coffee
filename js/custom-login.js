a56cdc6a92803c0b7b4c93b653318bc3e0eabd57
Jay0906 <jordan81hoc@gmail.com>
2026-08-18 12:55:04 -0400

    changed some wording

--- js/custom-login.js
+++ js/custom-login.js
@@ -22,9 +22,9 @@ function onFormSubmit(event) {
     console.log("Name:", name);
     console.log("Email:", email);
    console.log("password", pass);
-     let message = dataObject.login-message
-    message.textContent = `Welcome, ${name}! You are logged in!`
-    console.log(message);
+     let myMessage = dataObject.myMessageessage
+    myMessage.textContent = `Welcome, ${name}! You are logged in!`
+    console.log(myMessage);
 }
 
 form.addEventListener("submit", onFormSubmit);
