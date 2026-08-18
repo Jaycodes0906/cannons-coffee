35d6c4f7e7c270f314450714dfaf160700a8162c
Jay0906 <jordan81hoc@gmail.com>
2026-08-18 14:14:45 -0400

    changed codes

--- js/custom-inquiry.js
+++ js/custom-inquiry.js
@@ -1,5 +1,5 @@
 const form = document.getElementById("inquiryForm");
-const message = document,querySelector(".inquiry-message")
+const message = document,querySelector(".inquiry-message");
 
 
 function onFormSubmit(event) {
@@ -34,5 +34,5 @@ function onFormSubmit(event) {
 
     message.textContent = `Thank you ${name}! We appreciate your interest in ${inquiry} or ${hire}. Your coffee flavor is ${coffee} and your comments are ${comments}. We will soon contact you at ${email} or ${phone}.`
     
-
+}
 form.addEventListener("submit", onFormSubmit);
