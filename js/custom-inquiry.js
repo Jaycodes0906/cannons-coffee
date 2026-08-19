bb9d55e0221ba82ad23a546a10dca10f0b6c9448
Jay0906 <jordan81hoc@gmail.com>
2026-08-19 12:49:59 -0400

    changed some codes

--- js/custom-inquiry.js
+++ js/custom-inquiry.js
@@ -17,22 +17,20 @@ function onFormSubmit(event) {
 
     let name = dataObject.name
     let email = dataObject.email
-    let tel = dataObject.phoneNumber
+    let tel = dataObject.phone
     let comments = dataObject.comments
-    let buy = dataObject.buy
-    let employment = dataObject.employment
-    let flavor = dataObject.flavor
+    let buyEmploy = dataObject.buyEmploy
+    let flavor = dataObject.coffee
     
     console.log("Name:", name);
     console.log("Email:", email);
-   console.log("phone", phone);
+   console.log("phone", tel);
    console.log("comments", comments);
-   console.log("buy", buy);
-   console.log("employment", employment)
+   console.log("buyEmploy", buyEmploy);
    console.log("flavor", flavor);
    
 
-    message.textContent = `Thank you ${name}! We appreciate your interest in ${buy} or ${employment}. Your coffee flavor is ${flavor} and your comments are ${comments}. We will soon contact you at ${email} or ${phone}.`
+    message.textContent = `Thank you ${name}! We appreciate your interest in ${buyEmploy}. Your coffee flavor is ${flavor} and your comments are ${comments}. We will soon contact you at ${email} or ${tel}.`
     
 }
 form.addEventListener("submit", onFormSubmit);
